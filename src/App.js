c63f074455acdb7a28421d18885c1b1ea1b9bcd2
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-10-05 09:47:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Card from './card';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Cart from './Cart';
 import { useState } from 'react';
 function App() {
-  
-  const details=[
-    {
-      image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7DiMlnKLbthai3duMq_2a2qO0ymAKpBWIul_abwqbywFPPStalSDVwo1u1LzTSQDWfZzHhufDlvyL_9NpziQiq0o0BKyW1Q",
+  const [details,setdetails] =useState(
+    [{
+      id:1,
+      image:"https://rukminim1.flixcart.com/image/416/416/ktketu80/mobile/z/r/x/iphone-13-mlph3hn-a-apple-original-imag6vzz4rt8t7gk.jpeg?q=70",
       name:"iphone",
       price:"65000",
-      isEnabled:true
     },
     {
-      image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7DiMlnKLbthai3duMq_2a2qO0ymAKpBWIul_abwqbywFPPStalSDVwo1u1LzTSQDWfZzHhufDlvyL_9NpziQiq0o0BKyW1Q",
+      id:2,
+      image:"https://rukminim1.flixcart.com/image/416/416/ktketu80/mobile/e/e/r/iphone-13-mlpj3hn-a-apple-original-imag6vpykhzqhmgh.jpeg?q=70",
       name:"ipad",
       price:"50000",
-      isEnabled:true
     },
    {
-      image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7DiMlnKLbthai3duMq_2a2qO0ymAKpBWIul_abwqbywFPPStalSDVwo1u1LzTSQDWfZzHhufDlvyL_9NpziQiq0o0BKyW1Q",
+      id:3,
+      image:"https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/r/2/r/apple-iphone-12-mini-dummyapplefsn-original-imafwgbfcswp9fh5.jpeg?q=70",
       name:"Redmi",
       price:"25000",
-      isEnabled:true
    },
    {
-      image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7DiMlnKLbthai3duMq_2a2qO0ymAKpBWIul_abwqbywFPPStalSDVwo1u1LzTSQDWfZzHhufDlvyL_9NpziQiq0o0BKyW1Q",
+      id:4,
+      image:"https://rukminim1.flixcart.com/image/416/416/kgiaykw0/mobile/4/b/f/apple-iphone-11-mhdn3hn-a-original-imafwqeps9gkq9zd.jpeg?q=70",
       name:"Realme",
       price:"30000",
-      isEnabled:true
    },
    {
-      image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7DiMlnKLbthai3duMq_2a2qO0ymAKpBWIul_abwqbywFPPStalSDVwo1u1LzTSQDWfZzHhufDlvyL_9NpziQiq0o0BKyW1Q",
+      id:5,
+      image:"https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/n/p/d/apple-iphone-12-mini-dummyapplefsn-original-imafwgbfwyzztfnh.jpeg?q=70",
       name:"Samsung",
       price:"19000",
-      isEnabled:true
    },
    {
-      image:"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT7DiMlnKLbthai3duMq_2a2qO0ymAKpBWIul_abwqbywFPPStalSDVwo1u1LzTSQDWfZzHhufDlvyL_9NpziQiq0o0BKyW1Q",
+      id:6,
+      image:"https://rukminim1.flixcart.com/image/416/416/kg8avm80/mobile/d/g/b/apple-iphone-12-mini-dummyapplefsn-original-imafwgbfhfevaajh.jpeg?q=70",
       name:"Nokia",
       price:"35000",
-      isEnabled:true
    }
-   ]
+  ])
   const [empty,fill]=useState([])
   let addtocart=(details)=>{
     fill([...empty,details])
-   
   }
-
+  let removefromcart=(details)=>{
+    const remove=empty.findIndex(object => object.id===details.id)
+    empty.splice(remove,1)
+    fill([...empty])
+  }
   return (
     <div className="container-md">
       <div className='row'>
@@ -58,21 +60,15 @@ function App() {
             {
               details.map((items)=>{
                 return (
-                <Card items={items} addtocart={addtocart}></Card>
+                <Card items={items} addtocart={addtocart} empty={empty}></Card>
                 )
               })
             }
-           
-          
+  
           </div>
         </div>
         <div className='col-md-4'>
-          
-            
-               <Cart list={empty}></Cart>
-            
-          
-        
+               <Cart list={empty} removefromcart={removefromcart}></Cart>
         </div>
       </div>
     </div>
