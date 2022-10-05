79d22751e7ed7a02b2b74ad6af8e4b927aa60219
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-10-05 09:48:09 +0530

    Update Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -1,28 +1,38 @@
+
 import React from 'react'
 
-function Cart({list}) {
+function Cart({list,removefromcart}) {
   return (
-    <div>
-        {
-        list.map((list1)=>{
-            return <div>
-                <p> {list1.name}</p>
-                <p> {list1.price}</p>
-                </div>
-                   
-        })
-         }
-        
-        <h1>Total:Rs.{
+    <>
+    <h1>Cart</h1>
+    {
+      list.length===0?<div>No items</div>:
+      <>
+      <ol class="list-group list-group-numbered">
+{
+      list.map((list1)=>{
+            return (
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+            <div class="fw-bold">{list1.name}</div>
+            {list1.price}   
+            </div>
+            <button onClick={()=>{
+              removefromcart(list1)
+            }}class="badge bg-primary rounded-pill">X</button>
+            </li>
+       )})
+}
+</ol>
+<h1>Total:Rs{
             list.reduce((prev,curr)=>{
                 return prev=parseInt(prev)+parseInt(curr.price) ;
             },0)
-
-
-}</h1>
-       </div>
-
-  )
+          }
+</h1>
+    
+    </> 
 }
-
-export default Cart
+  </>
+)}
+export default Cart;
